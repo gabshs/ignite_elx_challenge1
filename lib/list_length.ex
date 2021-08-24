b10c0af30a29aa931147d7a3a24b09f3a208ea57
GabsDC <gabriel.silva@deliverycenter.com>
2021-08-24 18:27:52 -0300

    Resolving challenge using recursion

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,13 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    count_items(list)
+  end
+
+  defp count_items(list, counter \\ 0)
+
+  defp count_items([], counter), do: counter
+
+  defp count_items([_head | tail], counter) do
+    count_items(tail, counter + 1)
   end
 end
